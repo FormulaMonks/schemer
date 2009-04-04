a16894a402f67a4d6bf657b73c25227c99e7a753
benalavi <benalavi@gmail.com>
2009-04-03 17:30:44 -0700

    cut the code-base in half (thanks to finding ActiveRecord's handy table_exists? method)

--- lib/schemer.rb
+++ lib/schemer.rb
@@ -15,37 +15,20 @@ module Schemer
   
   module InstanceMethods
     def initialize(*args)
-      setup_schema
+      update_schema
       super
     end
   
     private
   
-    def setup_schema
-      begin
-        ActiveRecord::Migration.table_structure(self.class.table_name)
-        update_schema
-      rescue ActiveRecord::StatementInvalid => e
-        if e.message.match(/Could not find table/)
-          create_schema
-        else
-          raise e
-        end
-      end
-    end
-        
     def update_schema
+      ActiveRecord::Migration.create_table(self.class.table_name) do |t|;end; unless self.class.table_exists?
       self.class.schema_columns.each do |column|
         ActiveRecord::Migration.add_column(self.class.table_name, column, :string) unless respond_to?(column.to_sym)
         (self.class.column_names - self.class.protected_columns).each do |column|
           ActiveRecord::Migration.remove_column(self.class.table_name, column) unless self.class.schema_columns.include?(column.to_s)
         end
       end
-    end
-  
-    def create_schema
-      ActiveRecord::Migration.create_table(self.class.table_name) do |t|;end;
-      update_schema
     end
   end
 end
